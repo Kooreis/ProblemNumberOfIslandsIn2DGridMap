4af074104b9776f855de7e765a6824fc31b272aa
Kooreis <kooreis.official@gmail.com>
2024-02-18 13:57:22 -0600

    This is the main function of the program. It initializes a 2D grid map and then calls the NumIslands function to calculate the number of islands. The result is then printed to the console.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a C# console application that calculates the number of islands in a 2D grid map. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
-
-```C#
 using System;
 
 class Program
@@ -17,44 +14,3 @@ class Program
 
         Console.WriteLine("Number of islands is: " + NumIslands(grid));
     }
-
-    public static int NumIslands(char[][] grid)
-    {
-        if (grid == null || grid.Length == 0)
-        {
-            return 0;
-        }
-
-        int numIslands = 0;
-        for (int i = 0; i < grid.Length; i++)
-        {
-            for (int j = 0; j < grid[i].Length; j++)
-            {
-                if (grid[i][j] == '1')
-                {
-                    numIslands += Dfs(grid, i, j);
-                }
-            }
-        }
-
-        return numIslands;
-    }
-
-    public static int Dfs(char[][] grid, int i, int j)
-    {
-        if (i < 0 || i >= grid.Length || j < 0 || j >= grid[i].Length || grid[i][j] == '0')
-        {
-            return 0;
-        }
-
-        grid[i][j] = '0';
-        Dfs(grid, i + 1, j);
-        Dfs(grid, i - 1, j);
-        Dfs(grid, i, j + 1);
-        Dfs(grid, i, j - 1);
-        return 1;
-    }
-}
-```
-
-This program uses Depth-First Search (DFS) to count the number of islands. It starts by iterating over each cell in the grid. If the cell value is '1' (land), it increments the island count and then uses DFS to mark all the connected land cells as '0' (water). This way, when it encounters the same land cell again, it won't count it as a new island.
