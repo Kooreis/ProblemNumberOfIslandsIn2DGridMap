c628d227a74cdcde93618f3d579ca11458de9814
Kooreis <kooreis.official@gmail.com>
2024-02-19 13:57:22 -0600

    This part of the script initializes a count variable to 0 and then iterates over each cell in the grid. If a cell is land (represented by '1'), it increments the count and calls the dfs method to sink the island.

--- solution.py
+++ solution.py
@@ -1,4 +1,7 @@
-class Solution:
-    def numIslands(self, grid):
-        if not grid:
-            return 0
+count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
